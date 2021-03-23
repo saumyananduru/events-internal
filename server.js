ce062bdb8d98f824cdd0e81ad68dac1eae2178e0
kfedorov2020 <kfedorov@deloitte.com>
2021-03-23 13:25:16 +0000

    one more event

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ app.use(bodyParser.json());
 const mockEvents = {
     events: [
         { title: 'an event', id: 1, description: 'something really cool' },
-        { title: 'another event', id: 2, description: 'something even cooler' }
+        { title: 'another event', id: 2, description: 'something even cooler' },
+        { title: 'one more event', id: 2, description: 'something even cooler' }
     ]
 };
 
